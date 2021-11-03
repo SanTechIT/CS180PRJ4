8cdc4ed977e5b694c415fd45f94b1df8ff26bba1
Richard Chang <chang794@purdue.edu>
2021-11-03 15:55:14 -0400

    Inital Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Main Class (Starts the service)
+ *
+ * @author chang794
+ * @version 0.1
+ */
+public class Main {
+    /**
+     * Main server loop/Runnable
+     *
+     * @param args Commnand Line Arguments
+     */
+    public static void main(String[] args) {
+        // Start Server
+    }
+
+}
