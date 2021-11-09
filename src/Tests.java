e3543548dd332cc2d85c97f6f71f7a8da6a373af
Richard Chang <chang794@purdue.edu>
2021-11-09 16:39:10 -0500

    Added Tests Class, included junit5

---
+++ src/Tests.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+    
+    @Test
+    public void testOne() {
+
+    }
+}
