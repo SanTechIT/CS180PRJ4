854d2a0366b2700d436813ea93adef8baf34b469
Richard Chang <chang794@purdue.edu>
2021-11-13 20:39:18 -0500

    Post

--- src/Post.java
+++ src/Post.java
@@ -176,12 +176,5 @@ public class Post {
         return posts;
     }
 
-    /**
-     * Teachers can view a dashboard that lists the most popular replies to each forum by votes
-     *
-     * @return
-     */
-     public
-
 
 }
