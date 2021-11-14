cd61d3480fa66ddd7b2cc50c9208f829791ab1d1
Richard Chang <chang794@purdue.edu>
2021-11-13 21:05:40 -0500

    Added Serialization with test data in /data

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.io.*;
+import java.sql.SQLSyntaxErrorException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -16,11 +19,21 @@ public class Main {
 
         // Load User List if exits
         // TODO DELETE ME
-        User.USER_LIST = new ArrayList<>();
-        User.USER_LIST.add(new Teacher("teacher", "teacher", "John"));
-        User.USER_LIST.add(new Student("student", "student", "Alice"));
+//        User.USER_LIST = new ArrayList<>();
+//        User.USER_LIST.add(new Teacher("teacher", "teacher", "John"));
+//        User.USER_LIST.add(new Student("student", "student", "Alice"));
+        System.out.println("Reading Data...");
+        try {
+            User.USER_LIST = (List<User>) readData("data/UserList");
+            Course.COURSE_LIST = (List<Course>) readData("data/CourseList");
+        } catch (IOException e){
+            // TODO
+        } catch (ClassNotFoundException e){
+            // TODO
+        }
+        System.out.println("Data has been read and loaded!");
 
-        // Load Course List if exits
+//         Load Course List if exits
         Course.COURSE_LIST = new ArrayList<>();
         Course.createCourse("MA165", User.USER_LIST.get(0));
         Course.createCourse("CS180", User.USER_LIST.get(0));
@@ -54,5 +67,45 @@ public class Main {
                     break;
             }
         } while (!input.equals("exit")); // Not Exit
+
+        System.out.println("Saving Data...");
+        // ON Exit Save Data
+        try {
+            writeData(User.USER_LIST, "data/UserList");
+            writeData(Course.COURSE_LIST, "data/CourseList");
+        } catch (IOException e) {
+            System.out.println(
+                    "An error has occured while trying to save the data: " + e.getMessage());
+        }
+        System.out.println("Data has been saved!");
+    }
+
+    /**
+     * With inspiration from https://www.geeksforgeeks.org/serialization-in-java/
+     *
+     * @param obj
+     * @throws FileNotFoundException
+     */
+    private static void writeData(Object obj, String filename) throws IOException {
+        ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(filename, false));
+        output.writeObject(obj);
+        output.close();
+    }
+
+    /**
+     * Reads
+     * With inspiration from https://www.geeksforgeeks.org/serialization-in-java/
+     *
+     * @param filename
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    private static Serializable readData(
+            String filename) throws IOException, ClassNotFoundException {
+        ObjectInputStream input = new ObjectInputStream(new FileInputStream(filename));
+        Serializable obj = (Serializable) input.readObject();
+        input.close();
+        return obj;
     }
 }
