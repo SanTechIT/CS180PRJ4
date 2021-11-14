8c512c60d3d21638021a8090f88e9781b1449b08
Richard Chang <chang794@purdue.edu>
2021-11-13 19:44:26 -0500

    User

--- src/User.java
+++ src/User.java
@@ -72,7 +72,7 @@ public abstract class User implements Serializable {
             System.out.println("Successfully Logged In!");
         }
 
-        user.loop(in);
+        user.loop(in);gi
     }
 
     public static void createAccount(Scanner in) {
