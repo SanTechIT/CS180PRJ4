8761a3d2cf4f136330f18c472bf80e0e1767fd9b
Richard Chang <chang794@purdue.edu>
2021-11-14 19:37:39 -0500

    basic testing framework

--- src/Tests.java
+++ src/Tests.java
@@ -1,13 +1,12 @@
-/*
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-*/
 
-import java.io.IOException;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -32,11 +31,13 @@ public class Tests {
 
         // make new User object, set static vars
         Discussion.DISCUSSION_LIST = new ArrayList<>();
-        Discussion.createDiscussion(Course.COURSE_LIST.get(0), "default discussion", User.USER_LIST.get(0));
+        Discussion.createDiscussion(Course.COURSE_LIST.get(0), "default discussion",
+                User.USER_LIST.get(0));
 
         Post.POST_LIST = new ArrayList<>();
         Post post0 = s.makeDiscussionReply("test post 0", Discussion.DISCUSSION_LIST.get(0));
-        Post post1 = s.makePostReply(Post.POST_LIST.get(0), "test post 1", Discussion.DISCUSSION_LIST.get(0));
+        Post post1 = s.makePostReply(Post.POST_LIST.get(0), "test post 1",
+                Discussion.DISCUSSION_LIST.get(0));
         Post post2 = s.makePostReply(post0, "test post 2", Discussion.DISCUSSION_LIST.get(0));
         s.makePostReply(post1, "test post 3", Discussion.DISCUSSION_LIST.get(0));
         s.makePostReply(post2, "test post 4", Discussion.DISCUSSION_LIST.get(0));
@@ -64,10 +65,34 @@ public class Tests {
                     break;
             }
         } while (!input.equals("exit")); // Not Exit
+        // return array of objects
     }
 
-    // @Test
+    @Test
     public void testOne() {
+        // Courtesy of
+        // https://stackoverflow.com/questions/6415728/junit-testing-with-simulated-user-input
+        // As well as RunLocalTest.java (Various)
+        String commands = "";
+        commands += "login\n";
+        commands += "student\n";
+        commands += "student\n";
+        commands += "logout\n";
+        commands += "exit\n";
+
+        // Setup IN/OUTPUT
+        ByteArrayInputStream in = new ByteArrayInputStream(commands.getBytes());
+        PrintStream ts = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        System.setIn(in);
+
+        // Run Program
+        main(new String[0]);
+
+        // Restore output, print output
+        System.setOut(ts);
+        System.out.println(out);
 
     }
 }
